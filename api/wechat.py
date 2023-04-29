b6dfdf6d7a34e9492d6fab0c77235b224ff4ce9c
MagicLizi <jiali@magiclizi.com>
2023-04-29 23:16:25 +0800

    cmd

--- api/wechat.py
+++ api/wechat.py
@@ -1,13 +1,15 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from util.log import logger
 router = APIRouter()
 
 
-@router.get("/verify")
-async def verify(signature: str, timestamp: int, nonce: str, echostr: str):
-    logger.info(f"{signature}")
-    logger.info(f"{timestamp}")
-    logger.info(f"{nonce}")
-    logger.info(f"{echostr}")
+@router.get("/cmd")
+async def verify_url(signature: str, timestamp: int, nonce: str, echostr: str):
     return HTMLResponse(content=echostr)
+
+
+@router.post("/cmd")
+async def deal_wechat_msg(request: Request):
+    logger.info(request.body())
+
