3dc3a985e1cb57a84e46e3269b816c4f67f39612
MagicLizi <jiali@magiclizi.com>
2023-04-30 10:08:56 +0800

    add user

--- llm/openai_util.py
+++ llm/openai_util.py
@@ -27,7 +27,6 @@ class OpenAIUtil:
             stream=stream,
             messages=messages
         )
-        logger.info(response)
         assistant_message = response.choices[0].message['content']
         return assistant_message
 
