f61edb7caba022dd4530e78efe2b2a4474c2d469
MagicLizi <jiali@magiclizi.com>
2023-05-07 03:09:43 +0800

    1

--- api/wechat.py
+++ api/wechat.py
@@ -156,12 +156,9 @@ async def deal_wechat_msg(request: Request):
                 msg_id = root.find('./MsgId').text
                 user_msg_id = f"{from_user_name}_{msg_id}"
                 asyncio.create_task(resp_gpt_msg(content, "", user_msg_id, from_user_name, token, from_user_name))
-                return HTMLResponse(content=get_return_str(from_user_name, to_user_name, f"思考中...请耐心等待... <a "
-                                                                                         f"href="
-                                                                                         f"f'http://aichat.magiclizi.com"
-                                                                                         f"/wechat/pay?"
-                                                                                         f"open_id={from_user_name}'>"
-                                                                                         f"测试支付</a>"))
+                return_str = "思考中...请耐心等待..."
+                test_link = "<a href='www.baidu.com'>测试支付</a>"
+                return HTMLResponse(content=get_return_str(from_user_name, to_user_name, return_str + test_link))
             else:
                 return HTMLResponse(
                     content=get_return_str(from_user_name, to_user_name, "你不要发除了文字以外的东西！！"))
