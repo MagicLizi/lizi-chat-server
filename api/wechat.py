36b3b931b7310df49e86068951b3a35bcc65891e
MagicLizi <jiali@magiclizi.com>
2023-05-07 02:42:19 +0800

    1

--- api/wechat.py
+++ api/wechat.py
@@ -248,7 +248,7 @@ async def try_pay(request: Request):
                            appId: $appId,
                            timestamp: $timestamp,
                            nonceStr: $nonceStr,
-                           signature: signature,
+                           signature: $signature,
                            jsApiList: [
                             'chooseWXPay'
                            ]
@@ -280,9 +280,11 @@ async def try_pay(request: Request):
                 </body>
                 </html>
             """
-        html_content = re.sub("$timestamp", rst['timeStamp'], html_content)
-        html_content = re.sub("$timestamp", rst['timeStamp'], html_content)
-        html_content = re.sub("$timestamp", rst['timeStamp'], html_content)
-        html_content = re.sub("$timestamp", rst['timeStamp'], html_content)
+        html_content = re.sub("\$appId", rst['appId'], html_content)
+        html_content = re.sub("\$timestamp", rst['timeStamp'], html_content)
+        html_content = re.sub("\$nonceStr", rst['nonceStr'], html_content)
+        html_content = re.sub("\$signature", rst['paySign'], html_content)
+        html_content = re.sub("\$package", rst['package'], html_content)
+        html_content = re.sub("\$$paySign", rst['paySign'], html_content)
         print(html_content)
         return HTMLResponse(content=html_content)
