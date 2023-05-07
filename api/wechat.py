b5a84363cbeec6d5da1f5e0378dbbedcf08f520c
MagicLizi <jiali@magiclizi.com>
2023-05-07 11:29:43 +0800

    user add

--- api/wechat.py
+++ api/wechat.py
@@ -188,8 +188,8 @@ async def deal_wechat_msg(request: Request):
 
 async def wechat_pre_order(open_id):
     # 生成order
-    fee = 3000
-    order_id = await Order.create_order(open_id, "subscribe_month", 3000)
+    fee = 1
+    order_id = await Order.create_order(open_id, "subscribe_month", fee)
     if order_id != -1:
         print(order_id)
         current_dir = os.getcwd()
